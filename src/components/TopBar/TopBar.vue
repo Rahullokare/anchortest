b6c4c683953ed77a3fdcbdf77fa280637fa61e44
hrutik <hrutik@gmail.com>
2021-08-11 20:08:35 +0530

    startup

---
+++ src/components/TopBar/TopBar.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="container-fluid">
+    <div class="top__header bg-dark d-flex justify-content-end">
+      <div class="right__navigation">
+        <a href="#">Home</a>
+        <a href="#">Help</a>
+        <a href="#">Teach</a>
+        <a href="#">Login</a>
+        <button class="btn btn-primary">Join for Free</button>
+      </div>
+    </div>
+    <div class="sujession__bar p-2 d-flex justify-content-between">
+      <a href="#">Graphic And Design</a>
+      <a href="#">Digital Marketing</a>
+      <a href="#">Music And Video</a>
+      <a href="#">Writing and Translation</a>
+      <a href="#">Music and Videos</a>
+      <a href="#">Programming</a>
+      <a href="#">Tech</a>
+    </div>
+    <hr />
+    <div
+      class="search__bar p-1 d-flex align-items-center justify-content-between"
+    >
+      <div class="search__bar--left">
+        <a href="#">Overview</a>
+        <a href="#">Description</a>
+        <a href="#">About the Seller</a>
+        <a href="#">Compare Packages</a>
+        <a href="#">Reviews</a>
+      </div>
+      <div class="search__bar--center">
+        <i class="fas fa-search"></i>
+        <input
+          type="text"
+          placeholder="Hellow There"
+          class="search__bar--input"
+        />
+        <button class="search__bar--btn">Search</button>
+      </div>
+      <div class="search__bar--right">
+        <div class="dropdown">
+          <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenu2"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Dropdown
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+            <button class="dropdown-item" type="button">Action</button>
+            <button class="dropdown-item" type="button">Another action</button>
+            <button class="dropdown-item" type="button">
+              Something else here
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container-fluid {
+  margin: 0;
+  padding: 0;
+}
+.top__header {
+  padding: 0.75rem 0 0.75rem 0 !important;
+}
+hr {
+  margin: 0;
+  padding: 0;
+}
+.right__navigation {
+  margin-right: 65px;
+}
+.right__navigation > a {
+  text-decoration: none;
+  color: white;
+  margin-right: 25px;
+}
+.sujession__bar {
+  margin-left: 45px;
+  /* border-bottom: 1px solid #62646a; */
+}
+.sujession__bar > a {
+  text-decoration: none;
+  color: #62646a;
+  font-size: 18px;
+}
+.sujession__bar > a:last-child {
+  margin-right: 2rem;
+}
+/* SEARCH BAR */
+
+.search__bar {
+  background-color: rgb(218, 218, 218);
+}
+.search__bar--left > a {
+  margin-left: 45px;
+  text-decoration: none;
+  color: #62646a;
+}
+.search__bar--left > a:not(:first-child) {
+  margin-left: 1rem;
+}
+.search__bar--center {
+  background-color: white;
+  margin-left: 1.5rem;
+  /* // border: 1px solid darkgray; */
+  padding: 5px 5px 5px 10px;
+  display: flex;
+  align-items: center;
+  border-radius: 10px;
+}
+.search__bar--center > i {
+  color: #62646a;
+  font-size: 16px;
+}
+.search__bar--input {
+  border: none;
+  padding: 4px;
+  outline: none;
+}
+.search__bar--btn {
+  background-color: #1dbf73;
+  color: white;
+  padding: 5px 10px 5px 10px;
+  border-radius: 5px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+}
+/* SEARCH BAR RIGHT */
+.search__bar--right {
+  margin-right: 20px;
+}
+</style>
---
+++ src/views/GigProfile.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="container-fluid gigprofile">
+    <div class="row">
+      <div class="col-7 d-flex justify-content-center">
+        <div class="gigprofile__img">
+          <p>
+            I will be your full stack web developer in react js nodejs firebase
+          </p>
+          <div class="gigprofile__user">
+            <img
+              src="https://fiverr-res.cloudinary.com/t_profile_thumb,q_auto,f_auto/attachments/profile/photo/5693cf4f9db5dc367ef4a2eabd326ab1-908547401616923209634/JPEG_20210328_142008_6520848740211920130.jpg"
+              alt=""
+              class="gigprofile__user--img"
+            />
+            <p>Elizabeth Keen</p>
+          </div>
+          <div class="gigprofile__slider">
+            <div
+              id="carouselExampleControls"
+              class="carousel slide slider__image"
+              data-ride="carousel"
+            >
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img
+                    class="d-block w-100"
+                    src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNvlondRYQro2kEhdfoS5rG2Xtgl27hHT1rg&usqp=CAU"
+                    alt="First slide"
+                  />
+                </div>
+                <div class="carousel-item">
+                  <img
+                    class="d-block w-100"
+                    src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNvlondRYQro2kEhdfoS5rG2Xtgl27hHT1rg&usqp=CAU"
+                    alt="Second slide"
+                  />
+                </div>
+                <div class="carousel-item">
+                  <img
+                    class="d-block w-100"
+                    src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNvlondRYQro2kEhdfoS5rG2Xtgl27hHT1rg&usqp=CAU"
+                    alt="Third slide"
+                  />
+                </div>
+              </div>
+              <a
+                class="carousel-control-prev"
+                href="#carouselExampleControls"
+                role="button"
+                data-slide="prev"
+              >
+                <span
+                  class="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a
+                class="carousel-control-next"
+                href="#carouselExampleControls"
+                role="button"
+                data-slide="next"
+              >
+                <span
+                  class="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-5 image__stick">
+        <div class="gig__pricing"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+/* GIGPROFILE IMAGE */
+.gigprofile__user--img {
+  height: 32px;
+  width: 32px;
+  border-radius: 50px;
+}
+.gigprofile {
+  margin: 25px 0 0 0;
+}
+.gigprofile__img {
+  width: 87%;
+}
+.gigprofile__img > p {
+  font-size: 28px;
+  margin-bottom: 0.5rem;
+  font-weight: 700;
+}
+.gigprofile__user {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.gigprofile__user > p {
+  margin-left: 10px;
+  margin-bottom: 0;
+  font-weight: bold;
+}
+.gigprofile__slider {
+  margin-top: 25px;
+  display: flex;
+  justify-content: center;
+}
+.slider__image {
+  width: 90%;
+  height: 450px;
+}
+/* GIGPROFILE IMAGE END */
+</style>
